a44bab4298450cf065b82f428f0765916f47bea7
Vivek <20bcs105@iiitdwd.ac.in>
2024-10-10 17:08:13 +0530

    vercel fix 3

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-app.use("/user", userRouter);
+// app.use("/user", userRouter);
 
 app.get('/',(req,res)=>{
   return res.send("Plunes task")
